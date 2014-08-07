387398df0eb0bf1488b384f5d4bb39c2b7ac95f5
Jorge Izquierdo <jorge@izqui.me>
2014-08-07 15:46:27 -0400

    Open github new issues when nothing is selected

--- TicketMaster.py
+++ TicketMaster.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin, webbrowser
-import os.path
+import os.path as path
+import subprocess, traceback
 
 import re
 
@@ -10,6 +11,13 @@ ISSUE_LINK_REGEX = (
 	')\]') # followed by a close bracket (outside the capturing group)
 
 ISSUE_REGEX = 'TODO\:?(.*)'
+
+SSH_PREFIX = 'git@github.com:'
+HTTPS_PREFIX = 'https://'
+GITHUB_PREFIX = 'github.com/'
+GIT_SUFFIX = '.git'
+
+TOKEN_URL = 'https://github.com/settings/tokens/new?scopes=repo,public_repo'
 
 class CreateissueCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -37,18 +45,41 @@ class CreateissueCommand(sublime_plugin.TextCommand):
 			self.create_new_issue()
 
 	def create_new_issue(self):
-		pass
+		url = GITHUB_PREFIX + self.get_github_repo() + "/issues/new"
+		open_url(url)
 
 	def push_issue(self, edit, point, title):
-		print(title, edit, point)
+
+		
+		# Write url in file
 		self.view.insert(edit, point, "aba zaba")
 
+	def get_github_repo(self):
+		file_dir = self.get_file_directory()
+		try: 
+			output = subprocess.check_output(["git", "ls-remote","--get-url"], cwd=file_dir)
+			output = output.decode("utf-8")
+			if output.startswith(HTTPS_PREFIX+GITHUB_PREFIX):
+				output = output[len(HTTPS_PREFIX)+len(GITHUB_PREFIX):]
+			elif output.startswith(SSH_PREFIX):
+				output = output[len(SSH_PREFIX):]
+			else:
+				panic("Upstream remote is not a github repository. Got " + output + " instead.")
+
+			return output[:-len(GIT_SUFFIX)-1]
+
+		except:
+			traceback.print_exc()
+			print(file_dir)
+			panic("Couldn't determine github repository based on ls-remote. " +
+				"Try running: git ls-remote --get-url in "+file_dir)
+
 	def get_file_directory(self):
-		name = self.view.file_name()
-		if not name:
+		filepath = self.view.file_name()
+		if not filepath:
 			panic("File hasn't been saved")
 
-		return path 
+		return path.dirname(filepath)
 
 def extract_issue_title(line):
 	matches = re.findall(ISSUE_REGEX, line)
@@ -65,7 +96,7 @@ def extract_issue_link(line):
 		return None
 
 def open_url(url):
-	webbrowser.open_new_tab('https://' + url)
+	webbrowser.open_new_tab(HTTPS_PREFIX + url)
 
 def panic(error):
 	sublime.error_message("Ticket Master Error: " + error)
