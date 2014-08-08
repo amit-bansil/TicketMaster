9bc42a8a7a3cafa883c994b463c892c676efd5b8
Jorge Izquierdo <jorge@izqui.me>
2014-08-07 20:58:38 -0400

    Create github issues working. Sorry for the SPAM.

--- TicketMaster.py
+++ TicketMaster.py
@@ -1,8 +1,14 @@
-import sublime, sublime_plugin, webbrowser
+import sublime, sublime_plugin
 import os.path as path
-import subprocess, traceback
-
+import subprocess
+import webbrowser
+import traceback
+import http.client as httplib
+import base64
+import json
 import re
+
+from urllib.parse import urlparse
 
 ISSUE_LINK_REGEX = (
 	'\[(' + # capture everything after an open bracket
@@ -14,6 +20,7 @@ ISSUE_REGEX = 'TODO\:?(.*)'
 
 SSH_PREFIX = 'git@github.com:'
 HTTPS_PREFIX = 'https://'
+GITHUB_API_PREFIX = 'api.'
 GITHUB_PREFIX = 'github.com/'
 GIT_SUFFIX = '.git'
 
@@ -25,6 +32,9 @@ PREFERENCES_FILE = 'Preferences.sublime-settings'
 class CreateissueCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		
+		# Check if the user is already logged in.
+		print(self.get_github_token())
+
 		view = self.view
 		something_happened = False
 		for regionOfLines in view.sel():
@@ -53,9 +63,26 @@ class CreateissueCommand(sublime_plugin.TextCommand):
 
 	def push_issue(self, edit, point, title):
 
+		token = self.get_github_token()
+		url = HTTPS_PREFIX + GITHUB_API_PREFIX + GITHUB_PREFIX + "repos/" + self.get_github_repo() + "/issues"
 
-		# Write url in file
-		self.view.insert(edit, point, "aba zaba")
+		print(url)
+
+		res = authenticated_post(url, token, json.dumps({'title':title}))
+		res_body = res.read().decode('utf-8')
+		print(res_body)
+
+		if res.status == 201:
+			try:
+				res_params = json.loads(res_body) 
+				issue = res_params.get('html_url')
+				print(issue)
+				# Write url in file
+				self.view.insert(edit, point, ' [{}]'.format(issue[len(HTTPS_PREFIX):]))
+			except:
+				panic("Crash reading github JSON")
+		else:
+			panic("Problem connecting to Github: (Error {0}) {1}".format(res.status, res_body))
 
 	def get_github_repo(self):
 		file_dir = self.get_file_directory()
@@ -88,19 +115,9 @@ class CreateissueCommand(sublime_plugin.TextCommand):
 		s = sublime.load_settings(PREFERENCES_FILE)
 		t = s.get(TOKEN_KEY, None)
 
-		if t:
-			return t
-		else:
-
-			open_url(TOKEN_URL)
-
-	def save_github_token(self, token):
-		pass
-
-class RemovetokenCommand(sublime_plugin.ApplicationCommand):
-	def run(self, edit):
-		s = sublime.load_settings(PREFERENCES_FILE)
-		s.erase()
+		if not t:
+			panic("You need to setup Ticket Master running the Setup command.")
+		return t
 
 def extract_issue_title(line):
 	matches = re.findall(ISSUE_REGEX, line)
@@ -122,3 +139,57 @@ def open_url(url):
 def panic(error):
 	sublime.error_message("Ticket Master Error: " + error)
 	raise Exception(error)
+
+class SetuptokenCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		sublime.message_dialog("You are being redirected to Github. Create a token with that scope and paste it in the box below.")
+		self.window.show_input_panel("Your github token: ", "", self.save, None, None)
+		open_url(TOKEN_URL)
+
+	def save(self, token):
+		s = sublime.load_settings(PREFERENCES_FILE)
+		t = s.set(TOKEN_KEY, token)
+		sublime.save_settings(PREFERENCES_FILE)
+
+class RemovetokenCommand(sublime_plugin.ApplicationCommand):
+	def run(self):
+		s = sublime.load_settings(PREFERENCES_FILE)
+		s.erase(TOKEN_KEY)
+		sublime.save_settings(PREFERENCES_FILE)
+
+def request(method, url, options=None):
+	options = options or {}
+
+	url = urlparse(url)
+	host = url.netloc
+	path = url.path
+
+	if options.get('ssl'):
+		conn = httplib.HTTPSConnection(host)
+	else:
+		conn = httplib.HTTPConnection(host)
+
+	body = options.get('body', options.get('params', ''))
+	headers = options.get('headers', {})
+
+	conn.request(method, path, body, headers)
+	return conn.getresponse()
+
+def authenticated_post(url, token, params=None):
+	params = params or {}
+
+	print('{0}:{1}'.format(token, ''))
+	print('{0}:{1}'.format(token, '').encode('utf-8'))
+
+	encoded_userpass = base64.b64encode('{0}:{1}'.format(token, '').encode('utf-8')) #TODO fix bug
+	auth_string = 'Basic {0}'.format(encoded_userpass.decode('utf-8'))
+
+	options = {'params': params,
+			   'ssl': True,
+			   'headers': {'Authorization': auth_string,
+						   'Content-type': 'application/x-www-form-urlencoded',
+						   'User-Agent': 'ticketmaster'}}
+
+	print(options)
+
+	return request('POST', url, options)
